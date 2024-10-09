82a8e4adb8b2a64025e581de1d68665048b06f29
fazil6126912 <164895007+fazil6126912@users.noreply.github.com>
2024-10-09 17:03:25 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import Aside from "./Aside";
+import MainSection from "./mainSection";
+import Menu from "./menu";
+import { HEADERS } from "../constants";
+import { useState } from "react";
+
+function App() {
+  const [headers, setHeaders] = useState(HEADERS);
+  return (
+    <div className="flex h-screen text-white bg-bg font-body">
+      <Menu />
+      <MainSection headers={headers} />
+      <Aside headers={headers} setHeaders={setHeaders} />
+    </div>
+  );
+}
+
+export default App;
+
